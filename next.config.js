14835e4edf661cc30272f22ac848704f6c7a69fc
xoshimaljon-crypto <xoshimaljon@gmail.com>
2026-09-10 18:33:03 +1000

    bb

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  outputFileTracingIncludes: {
-    '/api/**/*': ['./products.db'],
+  experimental: {
+    outputFileTracingIncludes: {
+      '/api/**/*': ['./products.db'],
+    },
   },
 };
 
